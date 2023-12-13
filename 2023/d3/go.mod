d99a027659656897ec44ab8555dcd5366eb5d652
Nivekithan S <nivekithan363@gmail.com>
2023-12-13 20:29:27 +0530

    d3p2

--- 2023/d3/go.mod
+++ 2023/d3/go.mod
@@ -3,3 +3,5 @@ module d3
 go 1.21.5
 
 require github.com/deckarep/golang-set/v2 v2.5.0
+
+require github.com/google/uuid v1.5.0 // indirect
--- 2023/d3/main.go
+++ 2023/d3/main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+
+	"github.com/google/uuid"
 )
 
 func read_input() *[][]rune {
@@ -75,6 +77,88 @@ func par1() {
 	fmt.Println(sum)
 }
 
+func par2() {
+
+	content := read_input()
+
+	max_pos := Pos{row: len(*content) - 1, col: len((*content)[0]) - 1}
+
+	board := make([]string, (max_pos.row+1)*(max_pos.row+1))
+	id_to_num := map[string]int{}
+
+	engine_iterator := NewEngineIterator(content)
+
+	for engine_iterator.findCanContine() {
+		engine_iterator.SkipDot()
+
+		row := engine_iterator.cur_pos.row
+		num, num_cols, err := engine_iterator.ParseNum()
+
+		if err != nil {
+			engine_iterator.next()
+			continue
+		}
+
+		is_part_number := false
+
+		for _, col := range *num_cols {
+			pos := Pos{row: row, col: col}
+
+			if is_part_pos(pos, max_pos, content) {
+				is_part_number = true
+				break
+			}
+		}
+
+		if is_part_number {
+			rand_id := uuid.NewString()
+			id_to_num[rand_id] = num
+			for _, col := range *num_cols {
+				board[(row*(max_pos.row+1))+col] = rand_id
+			}
+		}
+	}
+
+	engine_iterator = NewEngineIterator(content)
+
+	sum := 0
+	for ; engine_iterator.findCanContine(); engine_iterator.next() {
+		cur_rune := engine_iterator.cur()
+
+		if cur_rune != '*' {
+			continue
+		}
+
+		all_adjs := get_adjacent(Pos{row: engine_iterator.cur_pos.row, col: engine_iterator.cur_pos.col}, max_pos)
+
+		all_parts := map[string]bool{}
+		for _, pos := range all_adjs {
+			part_id := board[(pos.row*(max_pos.row+1) + (pos.col))]
+
+			if part_id != "" {
+				all_parts[part_id] = true
+			}
+		}
+
+		all_part_num := []int{}
+		for k := range all_parts {
+
+			part_num := id_to_num[k]
+
+			if part_num != 0 {
+				all_part_num = append(all_part_num, part_num)
+			}
+		}
+
+		if len(all_part_num) == 2 {
+			sum += (all_part_num[0] * all_part_num[1])
+		}
+
+	}
+
+	fmt.Println(sum)
+}
+
 func is_part_pos(pos Pos, max_pos Pos, content *[][]rune) bool {
 	adjs := get_adjacent(pos, max_pos)
 
@@ -139,5 +223,5 @@ func get_adjacent(cur_pos, max_pos Pos) []Pos {
 }
 
 func main() {
-	par1()
+	par2()
 }
